18db31e865e3cdb2b21c3ddfdad36ad078968488
lee jeongmin <danmin20@khu.ac.kr>
2020-12-27 15:42:59 +0900

    initial setting

---
+++ src/pages/LoginPage.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Header } from "../components";
+
+const LoginPage = () => {
+  return (
+    <div>
+      <Header /> login
+    </div>
+  );
+};
+
+export default LoginPage;
---
+++ src/pages/MainPage.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Header } from "../components";
+
+const MainPage = () => {
+  return (
+    <div>
+      <Header /> login
+    </div>
+  );
+};
+
+export default MainPage;
