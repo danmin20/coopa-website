d1264747accc5ce7d305382e30d8460da73c3a21
lee jeongmin <danmin20@khu.ac.kr>
2021-01-03 22:13:35 +0900

    feat: router setting

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { LoginPage, MainPage } from "./pages";
+import DirDetail from "./pages/DirDetail";
 
 function App() {
   const isLoggined = true;
 
-  return <div className="App">{isLoggined ? <LoginPage /> : <MainPage />}</div>;
+  return (
+    <Router>
+      <Switch>
+        <Route path="/" exact>
+          {isLoggined ? <LoginPage /> : <MainPage />}
+        </Route>
+        <Route path="/directory/:dirId/:userId" exact>
+          <DirDetail />
+        </Route>
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
